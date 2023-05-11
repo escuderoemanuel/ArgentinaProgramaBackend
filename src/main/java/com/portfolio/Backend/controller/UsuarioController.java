3c6a58984a4e711918f056019f677a46ddbe215d
Emanuel Escudero <escudero.mdz@gmail.com>
2023-05-11 15:43:39 -0300

    Agregado addUsuario

--- src/main/java/com/portfolio/Backend/controller/UsuarioController.java
+++ src/main/java/com/portfolio/Backend/controller/UsuarioController.java
@@ -1,11 +1,11 @@
 package com.portfolio.Backend.controller;
 
+import com.portfolio.Backend.models.Educacion;
 import com.portfolio.Backend.service.UsuarioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.portfolio.Backend.models.Usuario;
-
 
 @RestController
 @RequestMapping("/usuario")
@@ -17,23 +17,27 @@ public class UsuarioController {
   public UsuarioController(UsuarioService usuarioService) {
     this.usuarioService = usuarioService;
   }
-  
-  @GetMapping("/saludar")
-  public String saludar() {
-    return "Funcionando!!!";
+
+  // Crear usuario
+  @PostMapping("/crear")
+  @ResponseBody
+  public Usuario crearUsuario(@RequestBody Usuario usuario) {
+    return usuarioService.addUsuario(usuario);
   }
 
+// Traer usuario por ID
   @GetMapping("/id/{id}")
   public ResponseEntity<Usuario> obtenerUsuario(@PathVariable("id") Long id) {
     Usuario usuario = usuarioService.buscarUsuarioPorId(id);
     return new ResponseEntity<>(usuario,
-        HttpStatus.OK);
+            HttpStatus.OK);
   }
 
+  // Actualizar usuario por ID
   @PutMapping("/update")
   public ResponseEntity<Usuario> editarUsuario(@RequestBody Usuario usuario) {
     Usuario updateUsuario = usuarioService.editarUsuario(usuario);
     return new ResponseEntity<>(updateUsuario,
-        HttpStatus.OK);
+            HttpStatus.OK);
   }
 }
