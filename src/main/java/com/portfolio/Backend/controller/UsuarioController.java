839d23e560932363009fd6eb5d615638c6a0d482
Emanuel Escudero <escudero.mdz@gmail.com>
2023-05-11 17:32:01 -0300

    Prueba de crearUsuario

--- src/main/java/com/portfolio/Backend/controller/UsuarioController.java
+++ src/main/java/com/portfolio/Backend/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package com.portfolio.Backend.controller;
 
-import com.portfolio.Backend.models.Educacion;
 import com.portfolio.Backend.service.UsuarioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +18,19 @@ public class UsuarioController {
   }
 
   // Crear usuario
-  @PostMapping("/crear")
+  @PostMapping("/usuario")
   @ResponseBody
   public Usuario crearUsuario(@RequestBody Usuario usuario) {
     return usuarioService.addUsuario(usuario);
+
   }
 
-// Traer usuario por ID
+  // Prueba
+  @GetMapping("/prueba")
+  public String saludar() {
+    return "Funcionando!!!";
+  }
+
   @GetMapping("/id/{id}")
   public ResponseEntity<Usuario> obtenerUsuario(@PathVariable("id") Long id) {
     Usuario usuario = usuarioService.buscarUsuarioPorId(id);
@@ -33,7 +38,6 @@ public class UsuarioController {
             HttpStatus.OK);
   }
 
-  // Actualizar usuario por ID
   @PutMapping("/update")
   public ResponseEntity<Usuario> editarUsuario(@RequestBody Usuario usuario) {
     Usuario updateUsuario = usuarioService.editarUsuario(usuario);
